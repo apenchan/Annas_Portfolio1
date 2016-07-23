88769e69e6670d3da1ceb25062cb4611ed4b6f3f
apenchan <a.penchansky@gmail.com>
2016-07-23 14:15:18 -0400

    just commit what I have so far

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    console.log("Hello world!");
+
+    $("a").click(function(){
+    	if(this.hash){
+    		var hash = this.hash.substr(1);
+    		console.log(hash);
+    		var $toElement = $("a[name=" + hash +"]");
+    		var toPosition = $toElement.position().top;
+
+    		$("body,html").animate({
+    			scrollTop : toPosition
+    		}, 2000)
+    	}
+    });
+});
